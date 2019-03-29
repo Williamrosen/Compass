adc9c41682645767823c1c21fcd869a61d0c95d3
williamrosenberg <william.rosenberg@live.se>
2019-03-29 15:50:51 +0100

    First Commit - Button in Main Activity leads to AccelerometerActivity

--- app/src/main/java/com/example/compass/MainActivity.java
+++ app/src/main/java/com/example/compass/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.compass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showCompass(View view) {
+        Intent intent = new Intent(this, AccelerometerActivity.class);
+        System.out.println("tjenna");
+        startActivity(intent);
+
+    }
+
 }
